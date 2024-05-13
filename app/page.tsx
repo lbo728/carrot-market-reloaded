7a489660e79cb99cd3ebb44b86216dc9b3183703
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-05-13 22:59:57 +0900

    4.0 Home Screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,27 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <span></span>
-        <h1>당근</h1>
-        <h2>당근 마켓에 어서오세요!!</h2>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center *:font-medium">
+        <span className="text-9xl">🥕🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!!</h2>
       </div>
-      <Link href="/crete-account">시작하기</Link>
-      <div>
-        <span>이미 계정이 있나요?</span>
-        <Link href="/login">로그인</Link>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/crete-account"
+          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div>
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
+``;
